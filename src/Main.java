dbaec529fd8e6a7fd02b0f7a3575b67bd0d2c67c
zeka qenberli <ganbarlizaka@gmail.com>
2024-04-05 09:40:00 +0400

    isiqfor

--- src/Main.java
+++ src/Main.java
@@ -3,49 +3,38 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//            Scanner scanner = new Scanner(System.in);
-//            System.out.println("reqemi daxil et");
-//            int fesi = scanner.nextInt();
-//
-//            switch (fesi) {
-//                case 1,2,3:
-//                    System.out.println("qis");
-//                    break;
-//                case 4,5,6:
-//                    System.out.println("yaz");
-//                    break;
-//                case 7,8,9:
-//                    System.out.println("yay");
-//                    break;
-//                case 10,11,12:
-//                    System.out.println("payiz");
-//                    break;
-//                default:
-//                    System.out.println("Yanlış giriş!");
-//            }
-//        }
-//    }
-
-
-        // 1-den n-dek ededler verilib n-i inputdan alin. O ededler icerisinde switch-case ile yoxlayin, 1-5
-        //arasinda olanlari taapdiqda, capa "5-10 arasindadir" yazin. Default-da da eded 10-dan boyukdur cixsin.
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("reqemi daxil edin");
-        int n = scanner.nextInt();
+        System.out.println("Rengi daxil edin");
+        String isiq = scanner.nextLine();
 
-        switch (n) {
-            case 1, 2, 3, 4, 5:
-                System.out.println("1-5 arasindadir");
+        switch (isiq) {
+            case "sari":
+                System.out.println("Hazirlasin");
                 break;
-            case 6,7,8,9,10:
-                System.out.println("5-10 arasindadir");
+
+            case "qirmizi":
+                System.out.println("Dayanin");
                 break;
+
+            case "yasil":
+                System.out.println("Hereket edin");
+                break;
+
             default:
-                System.out.println("eded 10-dan boyukdur");
+                System.out.println("Sfetefor islemir");
         }
-
-
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
